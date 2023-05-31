a3230529b56cbb8acc1a4eff4355819c29ae7a92
Mont Ramírez <montsesita78@gmail.com>
2023-05-30 23:49:06 -0600

    feat: a list was added

--- exercises.py
+++ exercises.py
@@ -1,3 +1,8 @@
 #Montserrat Guadalupe Ramírez Esparza   3°J
-#A list with 5 items, add a new element and print it using a for loop.
 
+#A list with 5 items, add a new element and print it using a for loop.
+print ("Below I will show a list of my friends from TIDSM:")
+friends = ["Sofi", "Max", "Jacob", "Erasmo", "Carlos"]
+friends.append("Marco")
+for friend in friends:
+    print(friend)
