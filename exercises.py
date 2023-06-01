fb6f404509b475ed199aa962bdebdc527c6673db
Mont Ramírez <montsesita78@gmail.com>
2023-06-01 13:10:30 -0600

    feat: a new function was added

--- exercises.py
+++ exercises.py
@@ -24,9 +24,36 @@ print ("******************")
 print ("Next I will show some of my personal information")
 mont = {"age": 20, "pets": 3, "name": "Montserrat"}
 mont["age"] = 19
-
 print ("Name: ", mont["name"])
 print ("Age: ", mont["age"])
 print ("Pets: ", mont["pets"])
 
 print ("******************")
+
+#A function named operation, that receives 3 params.
+
+print ("simple mathematical operations")
+
+def operation(first_number, operand, second_number):
+    if operand == "+":
+        addition = first_number + second_number
+        return addition
+    if operand == "-":
+        substract = first_number - second_number
+        return substract
+    if operand == "*":
+        multiply = first_number * second_number
+        return multiply
+    if operand == "/":
+        division = first_number / second_number
+        return division
+    else:
+        return "Syntax Error"
+        
+first_number = input("Write the first number: ")
+first_number = int(first_number)
+operand =  input("Write the operand sign: ")
+second_number = input("Write the second number: ")
+second_number = int(second_number)
+operation1 = operation(first_number, operand, second_number)
+print ( operation1)
