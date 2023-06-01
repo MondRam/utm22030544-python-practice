2824baadd51bca85a0bb3e55de01665200fe2164
Mont Ramírez <montsesita78@gmail.com>
2023-06-01 13:03:20 -0600

    feat: a new tuple was added

--- exercises.py
+++ exercises.py
@@ -5,4 +5,16 @@ print ("Below I will show a list of my friends from TIDSM:")
 friends = ["Sofi", "Max", "Jacob", "Erasmo", "Carlos"]
 friends.append("Marco")
 for friend in friends:
-    print(friend)
+    print (friend)
+print ("******************")
+
+#A tuple with 7 items and print it using a while loop.
+
+print ("Below is a list of the 7 most dangerous animals on the planet")
+
+danguerousAnimals = ("Crocodile", "Scorpions", "Assassin bugs", "Dogs(rabies)", "Snakes", "Elephants", "Mosquitoes")
+i = 0
+while i < len(danguerousAnimals):
+    print (danguerousAnimals[i])
+    i += 1
+print ("******************")
